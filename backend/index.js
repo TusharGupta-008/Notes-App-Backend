027cf58e05d427d5cafbdc8048da6680f16c4d7e
TusharGupta-008 <tushargupta.dsce@gmail.com>
2026-06-07 01:46:21 +0530

    creating the setup

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,16 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT;
+
+app.get("/",(req,res)=>{
+  res.send("hello")
+})
+
+app.listen(port, () => {
+  console.log(`Server has started on ${port}`);
+});
