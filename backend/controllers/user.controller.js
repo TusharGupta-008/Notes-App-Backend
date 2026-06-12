cde50319c097eac9036ce2475c897c280059ffcc
TusharGupta-008 <tushargupta.dsce@gmail.com>
2026-06-12 07:13:38 +0530

    profile route completed

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -76,8 +76,21 @@ const login = async (req, res) => {
 };
 
 const profileController = async (req, res) => {
-  console.log("test controller");
-  res.send("I'm in profileController");
+  try {
+    const user = await User.findById(req.user.id);
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+    return res.status(200).json({
+      user,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Server error",
+    });
+  }
 };
 
 export { signUp, login, profileController };
--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -1,6 +1,23 @@
+import  jwt  from "jsonwebtoken";
+
 const authMiddleware = async (req, res, next) => {
-  console.log("test for middleware");
-  next();
+  try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+      res.status(401).json({
+        message: "No token provided",
+      });
+    }
+    const token = authHeader.split(" ")[1];
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+     return res.status(401).json({
+      message: "Invalid token",
+    });
+  }
 };
 
 export default authMiddleware;
