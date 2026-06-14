a5cb8db357832341ae0468ad16771d8d28ceaca6
TusharGupta-008 <tushargupta.dsce@gmail.com>
2026-06-14 05:49:34 +0530

    pages related to react-router-dom

---
+++ frontend/src/pages/CreateNote.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CreateNote = () => {
+  return (<>Create Page</>);
+};
+
+export default CreateNote;
---
+++ frontend/src/pages/Sign.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Signup = () => {
+  return (<>Signup page</>);
+};
+
+export default Signup;
