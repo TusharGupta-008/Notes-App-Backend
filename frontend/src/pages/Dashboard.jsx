6563a81c72afe7dcde9bb0f17d80097bef3e6919
TusharGupta-008 <tushargupta.dsce@gmail.com>
2026-06-16 04:33:17 +0530

    dashboard page

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,47 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Dashboard = () => {
-  return (<>Dashboard page</>);
+  const [notes, setNotes] = useState([]);
+
+  const fetchNotes = async () => {
+    try {
+      const token = localStorage.getItem("token");
+
+      const response = await axios.get(
+        "http://localhost:4000/api/notes",
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      console.log(response.data);
+
+      setNotes(response.data.notes);
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  return (
+    <>
+      <h1>Dashboard</h1>
+
+      {notes.map((note) => (
+        <div key={note._id}>
+          <h2>{note.title}</h2>
+          <p>{note.description}</p>
+          <hr />
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default Dashboard;
